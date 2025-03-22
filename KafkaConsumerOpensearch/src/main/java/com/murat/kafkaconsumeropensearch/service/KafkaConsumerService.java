57b4c08598b442e87e6baa75a2814521dd0c5090
Murat Ali <murat.alkan2003@hotmail.com>
2025-03-22 11:37:39 +0300

    OpenSearch KafkaConsumer Oluşturuldu

---
+++ KafkaConsumerOpensearch/src/main/java/com/murat/kafkaconsumeropensearch/service/KafkaConsumerService.java
@@ -0,0 +1,19 @@
+package com.murat.kafkaconsumeropensearch.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+
+
+
+@Service
+public class KafkaConsumerService {
+    private static final Logger kafkaLogger = LogManager.getLogger("KafkaLogger");
+
+    @KafkaListener(topics = "rates_topic", groupId = "rate-group")
+    public void listen(String message) {
+        kafkaLogger.info(message);
+    }
+}
